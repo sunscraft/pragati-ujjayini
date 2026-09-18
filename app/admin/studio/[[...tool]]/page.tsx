4c055ccdb8bb1e9ea62f565ecf6a8f3415d56f84
Ayush <kandwalayush22@gmail.com>
2026-09-18 14:02:18 +0530

    ss

--- app/admin/studio/[[...tool]]/page.tsx
+++ app/admin/studio/[[...tool]]/page.tsx
@@ -3,6 +3,10 @@
 import { NextStudio } from 'next-sanity/studio';
 import config from '../../../../sanity.config';
 
+export const dynamic = 'force-dynamic';
+
 export default function StudioPage() {
   return <NextStudio config={config} />;
 }
+
+
